47e93de195c5388786c96063f876cd0f1ac84453
Tanguy ROYERE <t.royere@alptis.fr>
2023-09-11 14:28:40 +0200

    chore: assets sub folders

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/images/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -21,7 +21,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style lang="scss">
-@use "./assets/main.scss";
+@use "./assets/scss/main.scss";
 
 header {
   line-height: 1.5;
