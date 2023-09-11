7b9b128dbd3c00a84a0d0cad0077156d960f29b1
Tanguy ROYERE <t.royere@alptis.fr>
2023-09-11 19:07:02 +0200

    feat: init home page

---
+++ src/components/ThePresentation.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import homeImg from '../assets/images/screenshots/overworld/2023-09-10_15.12.59.webp';
+</script>
+
+<template>
+  <div class="the-presentation">
+    <div class="the-presentation__start">
+      <img :src="homeImg" class="the-presentation__start-background" />
+
+      <div class="the-presentation__overlay">
+        <header class="the-presentation__header">
+          <h1 class="the-presentation__title">Better Terra</h1>
+
+          <span class="the-presentation__sub-title">Coming soon !</span>
+        </header>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.the-presentation {
+  &__start {
+    display: flex;
+  }
+
+  &__start-background {
+    display: block;
+    max-width: 100%;
+    min-height: 100vh;
+    width: auto;
+    height: auto;
+    object-fit: cover;
+  }
+
+  &__overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+  }
+
+  &__header {
+    margin-top: 10%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #463D4B;
+  }
+
+  &__title {
+    font-family: "minecraft-ten";
+    font-size: 64px;
+  }
+
+  &__sub-title {
+    font-family: "pro-font-windows";
+    font-weight: bold;
+    font-size: 24px;
+    margin-top: 16px;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ThePresentation from '../components/ThePresentation.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ThePresentation />
   </main>
 </template>
