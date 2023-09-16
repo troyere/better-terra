39b4580bf8a24a6075b46e8cc1fad0a426d936e4
Tanguy Royere <tanguy.royere@gmail.com>
2023-09-16 14:57:15 +0200

    chore: test github.dev

--- src/components/TheInstallDoc.vue
+++ src/components/TheInstallDoc.vue
@@ -1,15 +1,28 @@
 <script setup lang="ts">
-
+import startScreenshot from '../assets/images/screenshots/2023-09-15_15.35.02.png';
 </script>
 
 <template>
   <div class="the-install-doc">
-
+    <div class="the-install-doc__start">
+      <img :src="startScreenshot" class="the-install-doc__start-background" />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 .the-install-doc {
+  &__start {
+    display: flex;
+  }
 
+  &__start-background {
+    display: block;
+    max-width: 100%;
+    min-height: 100vh;
+    width: auto;
+    height: auto;
+    object-fit: cover;
+  }
 }
 </style>
